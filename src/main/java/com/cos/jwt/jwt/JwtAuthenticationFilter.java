ef124a5190860a649a0988322a07d5d7ce563173
KingBlackCow <sgs1159@naver.com>
2021-12-10 17:55:06 +0900

    Add : jwt인증로직추가

--- src/main/java/com/cos/jwt/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/cos/jwt/jwt/JwtAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package com.cos.jwt.jwt;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.cos.jwt.auth.PrincipalDetails;
 import com.cos.jwt.model.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,10 +18,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Date;
 
 // 스프링 시큐리티에서 UsernamePasswordAuthenticationFilter가 있음.
 // /login 요청해서 username, password 전송하면 (post)
-// UsernamePasswordAuthenticationFilter 동작을함.
+// UsernamePasswordAuthenticationFilter 동작을 함.
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     private final AuthenticationManager authenticationManager;
@@ -39,7 +42,7 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 //            }
             ObjectMapper om = new ObjectMapper(); //json데이터 파싱시켜줌
             User user = om.readValue(request.getInputStream(), User.class);// user에 담아줌
-            System.out.println(user);
+            //System.out.println(user);
 
             UsernamePasswordAuthenticationToken authenticationToken =
                     new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword());
@@ -72,7 +75,19 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
                                             Authentication authResult) throws IOException, ServletException {
-        System.out.println("successfulAuthentication실행됨 : 인증이 완료되었다는뜻");
+        PrincipalDetails principalDetails = (PrincipalDetails) authResult.getPrincipal();
+
+        //Hash암호방식
+        String jwtToken = JWT.create()
+                .withSubject("cos토큰")
+                .withExpiresAt(new Date(System.currentTimeMillis()+(60000*10)))
+                .withClaim("id", principalDetails.getUser().getId())
+                .withClaim("username", principalDetails.getUser().getUsername())
+                .sign(Algorithm.HMAC512("cos"));
+
+        System.out.println("successfulAuthentication 실행됨 : 인증이 완료되었다는뜻");
         super.successfulAuthentication(request,response,chain,authResult);
+        System.out.println("Token: "+ jwtToken);
+        response.addHeader("Authorization","Bearer "+jwtToken);
     }
 }
